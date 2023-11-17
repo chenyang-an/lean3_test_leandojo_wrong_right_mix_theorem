76fd563379f8a6cf846cba89e2255478010bf93f
Chenyang <cya.portfolio@gmail.com>
2023-11-16 22:13:57 -0800

    add wrong theorem

--- src/example.lean
+++ src/example.lean
@@ -1,6 +1,12 @@
 open nat (add_assoc add_comm)
 
-theorem hello_world (a b c : ℕ) : a + b + c = a + c + b :=
+theorem correct_theoreom_with_proof (a b c : ℕ) : a + b + c = a + c + b :=
 begin
   rw [add_assoc, add_comm b, ←add_assoc]
 end
+
+theorem correct_theoreom_without_proof (a b c : ℕ) : a + b + c = a + c + b :=sorry
+
+
+theorem incorrect_theorem (a b c : ℕ :# a + b + c = a + c + b :=sorry
+
